2a54ff3c399df0c249f590113fc5e5752140b740
EC2 Default User <ec2-user@ip-172-31-11-45.ec2.internal>
2022-03-01 19:52:08 +0000

    lab6

---
+++ lec5.py
@@ -0,0 +1,53 @@
+'''
+lec5 if statement
+'''
+
+#import this
+
+#white spacing 
+print( 2+
+        1)
+
+print([ 1,2,3,
+        4,5,6])
+        
+m = 1+\
+        2
+print(m)
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(id([1,2,3])) #id equals identity 
+print(id(a))
+print(id(b))
+
+print(a is b)
+print(a == b)
+
+x = None # none is a datatype of its own that indicates no value 
+
+print(id(None))
+print(id(x))
+
+print (x == None)
+print(x is None)
+
+y = []
+print(y == None)
+print(y is None)
+
+#boolean type... true or false values
+print(True and False)
+print (True or False)
+print(not True)
+print(not False)
+
+print(not None)
+print(not '0')
+
+print(() and [])
+print([] and ())
+
+print(-1 or 0)
+print(0 or -1)
